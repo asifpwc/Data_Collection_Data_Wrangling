a241fa747ea0dd6ef90030ca6f36e174bfc4270a
Muhammad Asif <masifsharif@gmail.com>
2021-05-29 11:44:44 +0200

    merge & join data

---
+++ merge and join.R
@@ -0,0 +1,8 @@
+activiy = data.frame(opid = c("op01","op02","op03","op04","op05","op06","op07"),
+                     units = c(23,43,21,32,13,12,32))
+activiy
+names = data.frame(operator = c("Larry","Curly","Moe","Jack","Jill","Kim","Perry"))
+names
+cbind(activiy,names)
+df1 = data.frame(LETTERS,dfindex = 1:26)
+df2 <- data.frame(letters, dfindex = c(1:10,15,20,22:35))
